725974d21cacce2a4b09c4c77e02289bfa10e2e1
JENISHBN <jenishbn4@gmail.com>
2023-08-02 08:44:33 -0700

    updated

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class Employee {
+	private void empId() {
+		System.out.println(1234);
+	}
+	private void empAdd() {
+		System.out.println("jenish");
+
+	}	
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empId();
+		e.empAdd();
+	}
+		
+	
+
+	}
+
+
